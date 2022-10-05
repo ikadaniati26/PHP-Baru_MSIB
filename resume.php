1620cadbfb172272f7cf6ef6e7028adeccaebf62
ikadaniati26 <ikadaniati26@gmail.com>
2022-10-05 23:32:49 +0700

    tugas6 selesai

---
+++ resume.php
@@ -0,0 +1,31 @@
+<section id="resume" class="resume">
+      <div class="container" data-aos="fade-up">
+
+        <div class="row">
+          <div class="col-lg-6">
+           
+
+            <h3 class="resume-title">Education</h3>
+            <div class="resume-item">
+              <h4>MY JOURNEY STUDY</h4>
+              <h5>2006 - 2013</h5>
+              <p><em>SDN Sidowarek 2</em></p>
+            </div>
+            <div class="resume-item">
+              <h5>2015 - 2017</h5>
+              <p><em>SMPN 2 Plemahan</em></p>
+            </div>
+            <div class="resume-item">
+              <h5>2017 - 2020</h5>
+              <p><em>SMK Putra Harapan Plemahan | Otomasi Tata Kelola Perkantoran</em></p>
+            </div>
+            <div class="resume-item">
+              <h5>2017 - 2020</h5>
+              <p><em>Universitas Nusantara PGRI Kediri | Teknik Informatika</em></p>
+            </div>
+          </div>
+          
+        </div>
+
+      </div>
+    </section><!-- End Resume Section -->
